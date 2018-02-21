6bb7b352a9fe674736fdaae57e2c7cf84697b019
Christian Muehlhaeuser <muesli@gmail.com>
2018-02-21 02:18:00 +0100

    Set package.cpath so we can still execute frivpn from within the src dir

--- ovpn_client.lua
+++ ovpn_client.lua
@@ -1,3 +1,6 @@
+-- Make sure we can load libfrivpn when being executed from the src dir
+package.cpath = package.cpath .. ';./build/libfrivpn/?.so'
+
 local cqueues = require "cqueues"
 local csock = require "cqueues.socket"
 local hmac = require "openssl.hmac"
