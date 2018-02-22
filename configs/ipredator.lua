c086a9123b68bd74b8ab3081c11aa4efbf56001d
Christian Muehlhaeuser <muesli@gmail.com>
2018-02-22 07:56:37 +0100

    Added ipredator NAT service host to ipredator config example

--- configs/ipredator.lua
+++ configs/ipredator.lua
@@ -4,7 +4,10 @@ return {
 	cafile = "/etc/vpn/ipredator/ca.pem",
 	auth = "/etc/vpn/ipredator/ipredator.auth",
 	netmask = 24,
+	--ipredator regular service
 	host = "46.246.36.130",
+	--ipredator NAT service
+	--host = "46.246.46.100",
 	on_connected = "/etc/vpn/onconnect.sh",
 	--stats = true,
 	--debug = 0xffff,
